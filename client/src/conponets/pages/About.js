0fc95b3301c5d821ce962a88c6a0a6b681bdb487
Dioris Diaz <diorid@gmail.com>
2020-05-16 23:49:18 -0400

    navbar added

---
+++ client/src/conponets/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>About This App</h2>
+            <p className="my-1">
+                This is a Full Stack React app that keeps contacts
+            </p>
+            <p className="bg-light p">
+                <strong>Verson: </strong> 1.0.0
+            </p>
+        </div>
+    )
+}
+
+export default About
