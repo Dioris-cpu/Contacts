6b364ef37f23cd3672642f0626686e5d795f32ee
Dioris Diaz <diorid@gmail.com>
2020-09-15 22:56:05 -0400

    filter in use

--- client/src/conponets/contacts/ContactFilter.js
+++ client/src/conponets/contacts/ContactFilter.js
@@ -1,32 +1,36 @@
-// import React, { useContext, useRef, useEffect } from 'react'
-// import ContactContext from '../../context/contact/contactContext';
-// const ContactFilter = () => {
-//     const contactContext = useContext(ContactContext)
-//     const text = useRef('')
+import React, { useContext, useRef, useEffect } from 'react';
+import ContactContext from '../../context/contact/contactContext';
 
-//     const {filterContacts, clearFilter, filtered} = contactContext;
+const ContactFilter = () => {
+  const contactContext = useContext(ContactContext);
+  const text = useRef('');
 
-//     useEffect(() =>{
-//         if(filtered === null){
-//             text.current.value = '';
+  const { filterContacts, clearFilter, filtered } = contactContext;
 
-//         }
-//     })
+  useEffect(() => {
+    if (filtered === null) {
+      text.current.value = '';
+    }
+  });
 
-// const onChange = e =>{
-//     if(text.current.value != ''){
-//        filterContacts(e.target.value);
-//     }else{
-//        clearFilter();
-//     }
+  const onChange = e => {
+    if (text.current.value !== '') {
+      filterContacts(e.target.value);
+    } else {
+      clearFilter();
+    }
+  };
 
-// }    
-//     const contactContext = useContext(ContactContext);
-//     return(
-//         <form>
-//             <input ref={text} type='text' placeholder='Filter Contacts...' onChange={onChange}/>
-//         </form>
-//     )
-// }
+  return (
+    <form>
+      <input
+        ref={text}
+        type='text'
+        placeholder='Filter Contacts...'
+        onChange={onChange}
+      />
+    </form>
+  );
+};
 
-// export default ContactFilter
+export default ContactFilter;
